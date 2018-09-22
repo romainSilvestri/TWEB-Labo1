904ba7207b13d9063bcd11cb3e2aac55d33cf33e
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-09-22 19:04:01 +0200

    added js for follow/unfollow

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-console.log("as");
+let i = 0;
+
+function follow() {
+    if (i == 0) {
+        document.getElementById("button-follow").firstChild.data="Followed";
+        i = 1;
+    } else {
+        document.getElementById("button-follow").firstChild.data="Follow";
+        i = 0;
+    }
+}
