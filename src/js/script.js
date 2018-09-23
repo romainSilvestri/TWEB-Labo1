1e9c3ff0cb6e9234b8c25430afda3a535582483f
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-09-23 11:22:58 +0200

    changing button color on click

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,11 @@ let i = 0;
 function follow() {
     if (i == 0) {
         document.getElementById("button-follow").firstChild.data="Followed";
+        document.getElementById("button-follow").style.background="#3333ff";
         i = 1;
     } else {
         document.getElementById("button-follow").firstChild.data="Follow";
+        document.getElementById("button-follow").style.background="#71BD82";
         i = 0;
     }
 }
